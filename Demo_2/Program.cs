6fdbd2f189ab546a8fcd41e83b98181a02b4b456
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-06-30 22:01:47 +0300

    Q6

--- Demo_2/Program.cs
+++ Demo_2/Program.cs
@@ -51,8 +51,22 @@ namespace Demo_2
             int x = 10;
             int y = x;
             y = 20;
-            Console.WriteLine("x = " + x); // x will remain 10
-            Console.WriteLine("y = " + y); // y is 20
+            Console.WriteLine("x = " + x); 
+            Console.WriteLine("y = " + y);
+
+            #endregion
+            #region  Q6
+            /*
+             Write C# program that Assigning one reference type variable to
+            another and modifying the object through one variable 
+            and mention what will happen
+             
+             */
+            int[] arr1 = { 1, 2, 3 };
+            int[] arr2 = arr1;
+            arr2[0] = 100;
+
+            Console.WriteLine("arr1[0]: " + arr1[0]); 
 
             #endregion
         }
