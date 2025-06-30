ef5f67c00437f8cb6d3bf2dc69b88f9867536360
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-06-30 21:20:09 +0300

    Video_2

--- Demo_2/Program.cs
+++ Demo_2/Program.cs
@@ -8,7 +8,7 @@ namespace Demo_2
         {
             #region Video-1
             //float x= 2.5654831547988f;
-            //double y = 3.5;   
+            //double y = 3.5;     
             //decimal z = 4.5m;
             //Console.WriteLine(z);
 
@@ -17,6 +17,27 @@ namespace Demo_2
 
             var Calculatorinfo   = new CultureInfo("SA-ar");
             Console.WriteLine($"{a.ToString("C", Calculatorinfo)}");
+            #endregion
+
+            #region Video_2
+            // over flowing 
+            //unchecked { 
+            //long b = 100_000_000_0000;  
+            //    int Y=()int)b;
+            //    Console.WriteLine(Y);
+
+            //}
+
+            long E = 100_000_000_0000;
+            if (E > int.MaxValue || E < int.MinValue)
+            {
+                Console.WriteLine("The value is out of range for an int.");
+            }
+            else
+            {
+                int Y = (int)E;
+                Console.WriteLine(Y);
+            }
             #endregion
         }
     }
