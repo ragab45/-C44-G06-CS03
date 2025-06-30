555d73fa761bee76a242e351a6fb8bdd4ca6b328
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-06-30 20:24:07 +0300

    Video-1

--- Demo_2/Program.cs
+++ Demo_2/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Demo_2
+﻿using System.Globalization;
+
+namespace Demo_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Video-1
+            //float x= 2.5654831547988f;
+            //double y = 3.5;
+            //decimal z = 4.5m;
+            //Console.WriteLine(z);
+
+            long a = 100_000_000_0000;
+            Console.WriteLine($"{a:c}");
+
+            var Calculatorinfo   = new CultureInfo("SA-ar");
+            Console.WriteLine($"{a.ToString("C", Calculatorinfo)}");
+            #endregion
         }
     }
 }
